d9a64b55fc0f0d720234a36ba9c0086be3742e66
Subham Adhikari <subhamadhikari20@gmail.com>
2024-04-14 12:17:30 +0545

    Comments

--- Week1/HelloWorld.java
+++ Week1/HelloWorld.java
@@ -1,6 +1,9 @@
 package Week1;
 public class HelloWorld{
     public static void main(String[] args) {
+        // program to print Hello World
         System.out.println("Hello World");
+        
+        
     }
 }
